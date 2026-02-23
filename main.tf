5940dc65aca8f21364fc5bb9db49d55367b20186
Umar Arafath <umararafath@opstimus.com>
2026-02-23 12:12:15 +0530

    Consolidated nat instances resources to one block

--- main.tf
+++ main.tf
@@ -298,14 +298,14 @@ data "aws_ami" "amazon_linux_2" {
   owners = ["amazon"]
 }
 
-resource "aws_instance" "nat_1" {
-  count                       = var.nat == "instance" ? 1 : 0
+resource "aws_instance" "nat" {
+  count                       = var.nat == "instance" ? 3 : 0
   ami                         = data.aws_ami.amazon_linux_2.id
   instance_type               = var.nat_instance_type
   iam_instance_profile        = aws_iam_instance_profile.nat_instance[0].name
   vpc_security_group_ids      = [aws_security_group.nat_instance[0].id]
   source_dest_check           = false
-  subnet_id                   = aws_subnet.public_1.id
+  subnet_id                   = [aws_subnet.public_1.id, aws_subnet.public_2.id, aws_subnet.public_3.id][count.index]
   user_data_replace_on_change = true
   user_data = trimspace(
     <<EOF
@@ -313,9 +313,9 @@ resource "aws_instance" "nat_1" {
     sudo sysctl -w net.ipv4.ip_forward=1
     sudo /sbin/iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
     sudo yum -y install iptables-services
-    systemctl enable iptables
-    systemctl start iptables
     sudo service iptables save
+    sudo systemctl enable iptables
+    sudo systemctl start iptables
   EOF
   )
 
@@ -326,81 +326,10 @@ resource "aws_instance" "nat_1" {
 
   tags = merge(
     {
-      Name = "${var.project}-${var.environment}-nat-instance-1"
+      Name = "${var.project}-${var.environment}-nat-instance-${count.index + 1}"
     },
     var.tags
   )
-
-}
-
-resource "aws_instance" "nat_2" {
-  count                       = var.nat == "instance" ? 1 : 0
-  ami                         = data.aws_ami.amazon_linux_2.id
-  instance_type               = var.nat_instance_type
-  iam_instance_profile        = aws_iam_instance_profile.nat_instance[0].name
-  vpc_security_group_ids      = [aws_security_group.nat_instance[0].id]
-  source_dest_check           = false
-  subnet_id                   = aws_subnet.public_2.id
-  user_data_replace_on_change = true
-  user_data = trimspace(
-    <<EOF
-    #!/bin/bash
-    sudo sysctl -w net.ipv4.ip_forward=1
-    sudo /sbin/iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-    sudo yum -y install iptables-services
-    systemctl enable iptables
-    systemctl start iptables
-    sudo service iptables save
-  EOF
-  )
-
-  metadata_options {
-    http_endpoint = "enabled"
-    http_tokens   = "required"
-  }
-
-  tags = merge(
-    {
-      Name = "${var.project}-${var.environment}-nat-instance-2"
-    },
-    var.tags
-  )
-
-}
-
-resource "aws_instance" "nat_3" {
-  count                       = var.nat == "instance" ? 1 : 0
-  ami                         = data.aws_ami.amazon_linux_2.id
-  instance_type               = var.nat_instance_type
-  iam_instance_profile        = aws_iam_instance_profile.nat_instance[0].name
-  vpc_security_group_ids      = [aws_security_group.nat_instance[0].id]
-  source_dest_check           = false
-  subnet_id                   = aws_subnet.public_3.id
-  user_data_replace_on_change = true
-  user_data = trimspace(
-    <<EOF
-    #!/bin/bash
-    sudo sysctl -w net.ipv4.ip_forward=1
-    sudo /sbin/iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
-    sudo yum -y install iptables-services
-    systemctl enable iptables
-    systemctl start iptables
-    sudo service iptables save
-  EOF
-  )
-
-  metadata_options {
-    http_endpoint = "enabled"
-    http_tokens   = "required"
-  }
-
-  tags = merge(
-    {
-      Name = "${var.project}-${var.environment}-nat-instance-3"
-    },
-    var.tags
-  )
-
 }
 
 resource "aws_nat_gateway" "gateway_1" {
@@ -446,21 +375,21 @@ resource "aws_route" "private_1" {
   route_table_id         = aws_route_table.private_1.id
   destination_cidr_block = "0.0.0.0/0"
   nat_gateway_id         = var.nat == "gateway" ? aws_nat_gateway.gateway_1[0].id : null
-  network_interface_id   = var.nat == "instance" ? aws_instance.nat_1[0].primary_network_interface_id : null
+  network_interface_id   = var.nat == "instance" ? aws_instance.nat[0].primary_network_interface_id : null
 }
 
 resource "aws_route" "private_2" {
   route_table_id         = aws_route_table.private_2.id
   destination_cidr_block = "0.0.0.0/0"
   nat_gateway_id         = var.nat == "gateway" ? aws_nat_gateway.gateway_2[0].id : null
-  network_interface_id   = var.nat == "instance" ? aws_instance.nat_2[0].primary_network_interface_id : null
+  network_interface_id   = var.nat == "instance" ? aws_instance.nat[1].primary_network_interface_id : null
 }
 
 resource "aws_route" "private_3" {
   route_table_id         = aws_route_table.private_3.id
   destination_cidr_block = "0.0.0.0/0"
   nat_gateway_id         = var.nat == "gateway" ? aws_nat_gateway.gateway_3[0].id : null
-  network_interface_id   = var.nat == "instance" ? aws_instance.nat_3[0].primary_network_interface_id : null
+  network_interface_id   = var.nat == "instance" ? aws_instance.nat[2].primary_network_interface_id : null
 }
 
 resource "aws_eip" "nat_1" {
@@ -499,22 +428,10 @@ resource "aws_eip" "nat_3" {
   }
 }
 
-resource "aws_eip_association" "nat_instance_1" {
-  count         = var.nat == "instance" ? 1 : 0
-  instance_id   = aws_instance.nat_1[0].id
-  allocation_id = aws_eip.nat_1.id
-}
-
-resource "aws_eip_association" "nat_instance_2" {
-  count         = var.nat == "instance" ? 1 : 0
-  instance_id   = aws_instance.nat_2[0].id
-  allocation_id = aws_eip.nat_2.id
-}
-
-resource "aws_eip_association" "nat_instance_3" {
-  count         = var.nat == "instance" ? 1 : 0
-  instance_id   = aws_instance.nat_3[0].id
-  allocation_id = aws_eip.nat_3.id
+resource "aws_eip_association" "nat_instance" {
+  count         = var.nat == "instance" ? 3 : 0
+  instance_id   = aws_instance.nat[count.index].id
+  allocation_id = [aws_eip.nat_1.id, aws_eip.nat_2.id, aws_eip.nat_3.id][count.index]
 }
 
 data "aws_region" "current" {
